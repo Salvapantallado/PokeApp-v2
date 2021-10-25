201fef5a8adc172cb0dbee1eff6cddd4a08e17f1
lighuen <lighu.miranda@gmail.com>
2021-10-25 16:31:32 -0300

    Nodemon update9

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import Store from "./store/index";
+import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
+import App from "./App";
+import Store from "./store/index";
+import axios from "axios";
+import dotenv from "dotenv";
+
+import "./index.css";
 
 dotenv.config();
 
