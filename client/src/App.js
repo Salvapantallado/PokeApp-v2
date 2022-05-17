eaa0c9557c306bc542dd8a31b3ad961f7d141e37
Lighuen <lighu.miranda@gmail.com>
2022-05-17 20:31:15 -0300

    commit for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,12 @@ import { Route } from "react-router-dom";
 import { Home } from "./components/pages/Home/Home.js";
 import { LandingPage } from "./components/pages/LandingPage/LandingPage.js";
 import { Navbar } from "./components/component/Navbar/Navbar.js";
-import { SearchNav } from "./components/component/Navbar/SearchNav.js";
 import { PokeDetail } from "./components/pages/PokeDetail/PokeDetail.js";
 import { NewPoke } from "./components/pages/NewPoke/NewPoke.js";
 function App() {
   return (
     <React.Fragment>
       <Route path="/home" component={Navbar} />
-      <Route path="/search" component={SearchNav} />
       <Route path="/details/:id" component={Navbar} />
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/home" component={Home} />
