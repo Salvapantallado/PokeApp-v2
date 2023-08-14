10df329e74ea1d294edda5100ecc9d138eb8f59f
Lighuen <lighu.miranda@gmail.com>
2023-08-14 17:23:01 -0300

    images revamp v2

--- client/src/components/helpers/helper.jsx
+++ client/src/components/helpers/helper.jsx
@@ -1,94 +1,108 @@
-import icons from "./typeIcons";
+import dark from "./typeIcons/dark.png";
+import fire from "./typeIcons/fire.png";
+import water from "./typeIcons/water.png";
+import electric from "./typeIcons/electric.png";
+import grass from "./typeIcons/grass.png";
+import ice from "./typeIcons/ice.png";
+import fighting from "./typeIcons/fighting.png";
+import psychic from "./typeIcons/psychic.png";
+import poison from "./typeIcons/poison.png";
+import ground from "./typeIcons/ground.png";
+import bug from "./typeIcons/bug.png";
+import ghost from "./typeIcons/ghost.png";
+import dragon from "./typeIcons/dragon.png";
+import steel from "./typeIcons/steel.png";
+import fairy from "./typeIcons/fairy.png";
 
 export const colores = [
   {
     name: "normal",
     color: "#A8A77A",
-    icon: icons.dark,
+    icon: normal,
   },
   {
     name: "fire",
     color: "#EE8130",
-    icon: icons.fire,
+    icon: fire,
   },
   {
     name: "water",
     color: "#6390F0",
-    icon: icons.water,
+    icon: water,
   },
   {
     name: "electric",
     color: "#F7D02C",
-    icon: icons.electric,
+    icon: electric,
   },
   {
     name: "grass",
     color: "#7AC74C",
-    icon: icons.grass,
+    icon: grass,
   },
   {
     name: "ice",
     color: "#96D9D6",
-    icon: icons.ice,
+    icon: ice,
   },
   {
     name: "fighting",
     color: "#C22E28",
-    icon: icons.fighting,
+    icon: fighting,
   },
   {
     name: "poison",
     color: "#A33EA1",
-    icon: icons.poison,
+    icon: poison,
   },
   {
     name: "ground",
     color: "#E2BF65",
-    icon: icons.ground,
+    icon: ground,
   },
   {
     name: "flying",
     color: "#A98FF3",
-    icon: icons.flying,
+    icon: flying,
   },
   {
     name: "psychic",
     color: "#F95587",
-    icon: icons.psychic,
+    icon: psychic,
   },
   {
     name: "bug",
     color: "#A6B91A",
-    icon: icons.bug,
+    icon: bug,
   },
   {
     name: "rock",
     color: "#B6A136",
-    icon: icons.rock,
+    icon: rock,
   },
   {
     name: "ghost",
     color: "#735797",
-    icon: icons.ghost,
+    icon: ghost,
   },
   {
     name: "dragon",
     color: "#6F35FC",
-    icon: icons.dragon,
+    icon: dragon,
   },
   {
     name: "dark",
     color: "#736C75",
-    icon: icons.dark,
+    icon: dark,
   },
   {
     name: "steel",
     color: "#B7B7CE",
-    icon: icons.steel,
+    icon: steel,
   },
   {
     name: "fairy",
     color: "#D685AD",
-    icon: icons.fairy,
+    icon: fairy,
   },
 ];
